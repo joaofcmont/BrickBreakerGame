f99bdae26e46d0225cb4b69ac1b6a15e7352f3e8
joaomonteiro <jfcmo98647@gmail.com>
2024-10-12 23:33:23 +0100

    Adding a starting menuy

--- src/program/Application.java
+++ src/program/Application.java
@@ -1,16 +1,45 @@
 package program;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class Application extends JFrame {
 
     public Application(){
-        JFrame jFrame = new JFrame();
-        jFrame.setVisible(true);
-        jFrame.setSize(400,400);
-        jFrame.setTitle("Brick Breaker's");
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton startButton = new JButton("Start Game");
+        startButton.setVisible(true);
+        startButton.setFocusable(false);
+        JFrame startFrame = new JFrame();
+        startFrame.setLayout( new BorderLayout() );
+        startFrame.add(startButton, BorderLayout.CENTER);
+        startFrame.setVisible(true);
+        startFrame.setSize(400,400);
+        startFrame.setTitle("Brick Breaker's");
+        startFrame.setLocationRelativeTo(null);
+        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startFrame.dispose();
+                showGameWindow();
+            }
+
+        });
+
     }
 
+    private void showGameWindow(){
+        JFrame gameFrame = new JFrame();
+        gameFrame.setLayout(new BorderLayout());
+        gameFrame.setSize(400,400);
+        gameFrame.setVisible(true);
+        gameFrame.setLocationRelativeTo(null);
+        gameFrame.setTitle("Brick Breaker's");
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
+
+
