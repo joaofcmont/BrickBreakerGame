288f2b149ffdc053facefcb10bd6d0abd1056b18
joaomonteiro <jfcmo98647@gmail.com>
2024-10-12 23:10:00 +0100

    Adding JFrame of the game

---
+++ src/program/Application.java
@@ -0,0 +1,16 @@
+package program;
+
+import javax.swing.*;
+
+public class Application extends JFrame {
+
+    public Application(){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setSize(400,400);
+        jFrame.setTitle("Brick Breaker's");
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
